69250f2e10550685dce81e2095a1b1b7fb08b315
Enrique Marques Jr <enr@esaid.com.br>
2020-04-12 13:04:15 -0300

    added bass*

--- rap81-beat.rb
+++ rap81-beat.rb
@@ -1,38 +1,40 @@
 use_bpm 81
 
 live_loop :kick_and_snare do
-  8.times do
-    3.times do
-      sample :bd_808, rate: 1, amp: 4
-      sleep 1
-      sample :elec_hi_snare, amp: 1
-      sleep 1.5
-      sample :bd_808, rate: 1, amp: 4
-      sleep 0.5
-      sample :elec_hi_snare, amp: 1
-      sleep 1
-    end
-
+  3.times do
     sample :bd_808, rate: 1, amp: 4
     sleep 1
     sample :elec_hi_snare, amp: 1
     sleep 1.5
-    sample :elec_hi_snare, amp: 1
-    sleep 0.5
     sample :bd_808, rate: 1, amp: 4
-    sleep 0.25
+    sleep 0.5
     sample :elec_hi_snare, amp: 1
-    sleep 0.75
+    sleep 1
   end
+
+  sample :bd_808, rate: 1, amp: 4
+  sleep 1
+  sample :elec_hi_snare, amp: 1
+  sleep 1.5
+  sample :elec_hi_snare, amp: 1
+  sleep 0.5
+  sample :bd_808, rate: 1, amp: 4
+  sleep 0.25
+  sample :elec_hi_snare, amp: 1
+  sleep 0.75
 end
 
 live_loop :hihat do
-  8.times do
-    sample :drum_cymbal_closed, amp: 0.5
-    sleep 0.5
-    sample :drum_cymbal_pedal, amp: 0.5
-    sleep 1
-  end
+  sample :drum_cymbal_closed, amp: 0.5
+  sleep 0.5
+  sample :drum_cymbal_pedal, amp: 0.5
+  sleep 1
+end
+
+live_loop :bass do
+  use_synth :mod_beep
+  play 40
+  sleep 4
 end
 
 live_loop :luuppi do
